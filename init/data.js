aff18a66c5c5b32a30e4fc4ec4dadcd6e3c3e98a
PrathameshSarode7 <prathameshvbsarode1323@gmail.com>
2024-11-06 18:08:11 +0530

    first commit

---
+++ init/data.js
@@ -0,0 +1,45 @@
+const sampleListings=[
+    {
+        title:"delhi fg",
+        description:"qutubminar",
+        image:"https://unsplash.com/photos/gray-concrete-bridge-and-waterfalls-during-daytime-cssvEZacHvQ",
+        price:356,
+        location:"agra",
+        country:"india",
+    },
+    {
+        title:"tufani",
+        description:"bhurj khalifa",
+        image:"https://thumbs.dreamstime.com/b/pollution-random-place-found-very-silent-i-m-amazed-no-one-talks-179222851.jpg",
+        price:345,
+        location:"aljdkfs",
+        country:"tanta",
+    },
+    {
+        title:"dsf",
+        description:"rter",
+        image:"https://unsplash.com/photos/foggy-mountain-summit-1Z2niiBPg5A",
+        price:3453,
+        location:"adfsi",
+        country:"jjla",
+    },
+    {
+        title:"dkjgn",
+        description:"flasjg adsf",
+        image:"https://unsplash.com/photos/green-grass-field-during-sunset-_RBcxo9AU-U",
+        price:4534,
+        location:"adsf",
+        country:"brazil",
+    },
+    {
+        title:"sdgs",
+        description:"sdafaes",
+        image:"78051112.jpg",
+        price:78645,
+        location:"asdg",
+        country:"tatata",
+    },
+    
+
+];
+module.exports={data:sampleListings};
